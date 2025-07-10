c00d231d4a3e0a6d43d3a05a0327628336b108f2
DELSOBRINHO <delsobrinho@harmonia.com>
2025-07-09 22:55:22 -0300

    fix: remover importações desnecessárias de React em páginas de cliente, entregador e lavanderia

--- src/pages/lavanderia/HistoricoPedidos.tsx
+++ src/pages/lavanderia/HistoricoPedidos.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { getLaundryHistory } from '../../services/orderService';
 import { useAuth } from '../../contexts/AuthContext';
 
